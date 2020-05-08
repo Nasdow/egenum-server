3e0bd0a03f5dcc19bfbcbd52a5b7c4a7b3e53b85
Nasdow-92 <nassim.benelhadj@efrei.net>
2020-05-08 05:57:55 +0200

    EJS advanced tests

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const http = require("http");
 const bodyParser = require("body-parser");
@@ -10,7 +11,7 @@ const PORT = process.env.PORT || 5000
 var app = express();
 var server = http.createServer(app);
 
-const whiteList = ['http://localhost:8080', 'http://127.0.0.1:8080', 'https://egenum-admin.herokuapp.com']
+const whiteList = ['http://localhost:5000', 'http://127.0.0.1:5000', 'https://egenum-admin.herokuapp.com']
 const corsOptions = {
     origin: function (origin, callback) {
         if (!origin || whiteList.indexOf(origin) !== -1) {
@@ -24,13 +25,15 @@ const corsOptions = {
 app.use(cors(corsOptions))
 app.use(helmet())
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: true}))
 
+app.set('views', path.join(__dirname, "views"))
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+
+app.use(express.static(path.join(__dirname, "public")))
 
 router(app)
 
 server.listen(PORT, ()=> {
-    console.log("Server listening on 8080...");
+    console.log("Server listening on 5000...");
 })
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,30 @@ const utils = require("../utils");
 
 module.exports = (app) => {
     /** Entry point*/
-    app.get("/welcome", controllers.entry, utils.handleErrors)
+    app.get("/", (req, res) => {
+        res.render("index");
+    })
+
+    app.get("/login", (req, res) => {
+        res.render("login", {
+            data: {},
+            errors: {}
+        });
+    })
+    /**Middlewares: check input validity, check auth with AWS */
+    app.post("/login", (req, res) => {
+        res.render("login", {
+            data: req.body,
+            errors: {
+                userName: {
+                    msg: "Le nom d'utilisateur est incorrect !"
+                },
+                password: {
+                    msg: "Le mot de passe est incorrect !"
+                }
+            }
+        })
+    })
 
     /**Post data */
 
